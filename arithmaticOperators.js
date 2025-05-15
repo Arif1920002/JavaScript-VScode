f4b4ee1785052a81b4925970f990a4dc5f236f7d
S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>
2025-05-15 23:44:52 +0600

    Add arithmetic operators and update student count
    This commit adds the basic arithmetic operators and updates the number of students using assignment operators. The code now performs addition, subtraction, multiplication, division, and calculates the remainder for the number of students. The new count is then logged to the console.

---
+++ arithmaticOperators.js
@@ -0,0 +1,28 @@
+/* Arithmetic operators 
+
+Basic arithmetic operators: Addition (+) Subtraction (-) Multiplication (*) Division (/) Modulus (%) Exponentiation (**)
+
+Increment/decrement operators: Increment (++) Decrement (--)
+
+Assignment operators: Addition assignment (+=) Subtraction assignment (-=) Multiplication assignment (*=) Division assignment (/=) Modulus assignment (%=)   */
+
+
+let students1 = 30;
+let students2 = 20;
+let students3 = 10;
+let students4 = 51;
+let students5 = 22;
+
+students1 += 5; // students = students + 5
+students2 -= 3; // students = students - 2
+students3 *= 2; // students = students * 2
+students4 /= 2; // students = students / 2
+students5 %= 3; // students = students % 3
+
+
+
+console.log("new number of students after adding: " + students1);
+console.log("new number of students after subtracting: " + students2);
+console.log("new number of students after multiplying: " + students3);
+console.log("new number of students after dividing: " + students4);
+console.log("new number of students after remainder modules: " + students5);
