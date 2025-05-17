d418d57e2dc4bb9c51f9c137e78f4ed27cbe38d1
S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>
2025-05-17 18:11:44 +0600

    Adds user input script for interactive functionality
    A new script for handling user input is added to enhance interactivity on the webpage.

---
+++ userInput.js
@@ -0,0 +1,10 @@
+// How to take user input in JavaScript
+
+// 1. Easy way to take user input in JavaScript is using prompt() function.
+// 2. Professional way to take user input in JavaScript is using HTML form elements like <input>, <select>, <textarea>, etc. and then using JavaScript to get the values from these elements.
+
+
+let username;
+username = window.prompt("What's you name?");
+console.log(username);
+
