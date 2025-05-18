4210b56eeb21d4933c8b3f4a0ef42149634d237f
S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>
2025-05-18 15:33:32 +0600

    Updates type conversions for variables in typeConversion.js
    Converts variables to different data types for demonstration.

--- typeConversion.js
+++ typeConversion.js
@@ -4,3 +4,16 @@ let yourAge = window.prompt("How old are you?");
 yourAge = Number(yourAge); // convert string to number
 yourAge+=1;
 console.log(yourAge, typeof yourAge); // Output: 26 number
+
+
+let p1 = "pizza"; 
+let p2 = "pizza";
+let p3 = "pizza"; 
+
+p1 = Number(p1); // not a number, until its either 0 or empty string
+p2 = String(p2); // is always a string even if its empty
+p3 = Boolean(p3); // is false if its empty string, 0, null, undefined, NaN; otherwise true
+
+console.log(p1, typeof p1); 
+console.log(p2, typeof p2); 
+console.log(p3, typeof p3); 
