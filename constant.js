fdedc4077868b0c11ed2fd29db81782a5d1ff7ea
S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>
2025-05-18 20:54:08 +0600

    Adds constant.js to script loading in index.html
    Ensures constant.js is loaded at the end of the body in index.html for proper execution.

---
+++ constant.js
@@ -0,0 +1,11 @@
+// const = a variable that cannot be changed
+
+const PI = 3.14159;
+let radius;
+radius = Number(radius); // convert string to number
+
+radius = window.prompt("Enter the radius of the circle: ");
+
+let circumference = 2 * PI * radius;
+
+console.log("The circumference is:", circumference);
