567d0874df5254ce9ecb74816f6e8d7ab125f36f
S.M. ARIF HASAN <110717484+Arif1920002@users.noreply.github.com>
2025-05-18 14:13:44 +0600

    Adds typeConversion.js script for data type conversion
    Adds a new script for handling data type conversions in JavaScript to enhance flexibility and data manipulation capabilities.

---
+++ typeConversion.js
@@ -0,0 +1,6 @@
+// Type conversion = converting a datatype from one type to another
+
+let yourAge = window.prompt("How old are you?");
+yourAge = Number(yourAge); // convert string to number
+yourAge+=1;
+console.log(yourAge, typeof yourAge); // Output: 26 number
